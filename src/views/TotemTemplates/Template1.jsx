74c0d56ad1ce0603623899965e0f12e9b47d9179
Jose luis larrazabal <lfj1009416@est.univalle.edu>
2024-11-09 19:02:13 -0400

    Implementacion de tecnologias de control de respuesta para el usuario ya confirmamos si la busqueda es correcta y si no, se realizan nuevas busquedas, se implemento un cambio de disenho en la interfaz mas atractiva al totem

--- src/views/TotemTemplates/Template1.jsx
+++ src/views/TotemTemplates/Template1.jsx
@@ -17,14 +17,13 @@ import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet"
 import "leaflet/dist/leaflet.css";
 
 import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 // Tu clave de API de Mapbox
 const MAPBOX_API_KEY = 'pk.eyJ1IjoiYm9qamkwMCIsImEiOiJjbTJpY3EzeHIwbmFhMmlvbjV2NTVuejlwIn0.zvtpq8yNQYuUdEBUkYSUvw';
 
 // Configurando el icono de Leaflet
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-
 const DefaultIcon = L.icon({
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
@@ -37,10 +36,10 @@ const DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 export function Template1() {
-  const chatPDFApiKey = "sec_6Iv3eMYHKFN3Qkdwa6rF70GRcAaRgoK6";
+  const chatPDFApiKey = "sec_6Iv3eMYHKFN3Qkdwa6rF70GRcAaRgoK6"; // Clave de API para el chat de PDF
   const navigate = useNavigate();
   const location = useLocation();
-  const [browse, SetBrowse] = useState("");
+  const [browse, setBrowse] = useState("");
   const [imagesFinal, setImages] = useState(null);
   const [highlightedNavItem, setHighlightedNavItem] = useState(""); // Para resaltar el navbar
   const [destination, setDestination] = useState(null); // Para guardar la ubicación de destino
@@ -48,19 +47,21 @@ export function Template1() {
   const [loading, setLoading] = useState(true);
   const [routeCoords, setRouteCoords] = useState([]); // Coordenadas de la ruta
   const [keywords, setKeywords] = useState([]); // Ubicaciones dinámicas del navbar
+  const [awaitingConfirmation, setAwaitingConfirmation] = useState(false); // Controla la confirmación de ubicación
 
   const initialLocation = [-17.332983, -66.226246]; // Ubicación inicial de la app
 
   const { startListening, stopListening, isListening } = useSpeechRecognition(handleSubmit);
 
   useEffect(() => {
+    // Cargar archivos PDF y ubicaciones de la base de datos
     const fetchAndUploadFiles = async () => {
       await getPdfFiles(totem.idTotem, chatPDFApiKey);
       setLoading(false);
     };
     fetchAndUploadFiles();
 
-    // Nueva función para recuperar ubicaciones de la base de datos
+    // Recuperar ubicaciones de la base de datos
     const fetchLocations = async () => {
       try {
         const response = await axios.get(`${connectionString}/Ubicaciones`);
@@ -74,7 +75,22 @@ export function Template1() {
     fetchLocations(); // Llama a la función para recuperar ubicaciones
   }, [totem.idTotem, chatPDFApiKey]);
 
+  // Función para usar ResponsiveVoice
+  const speakMessage = (message) => {
+    if (typeof responsiveVoice !== "undefined") {
+      responsiveVoice.speak(message, "Spanish Latin American Female");
+    } else {
+      console.error("ResponsiveVoice no está disponible.");
+    }
+  };
+
+  // Función para manejar la búsqueda por voz
   function handleSubmit(textToSearch) {
+    if (awaitingConfirmation) {
+      handleConfirmationResponse(textToSearch);
+      return;
+    }
+
     let filteredKeys = textToSearch.split(" ").filter(item => keywords.includes(item));
 
     if (filteredKeys.length > 0) {
@@ -86,6 +102,7 @@ export function Template1() {
     }
   }
 
+  // Verifica si la ubicación reconocida está en la base de datos
   const checkLocationInDatabase = async (recognizedLocation) => {
     const formattedLocation = recognizedLocation.charAt(0).toUpperCase() + recognizedLocation.slice(1).toLowerCase();
 
@@ -95,14 +112,12 @@ export function Template1() {
       });
 
       if (response.data) {
-        console.log("Ubicación encontrada:", response.data); // Aquí se muestra la ubicación encontrada
-
         const latitud = response.data.latitud;
         const longitud = response.data.longitud;
 
         if (latitud && longitud) {
           setDestination([latitud, longitud]);
-          drawRoute([latitud, longitud]); // Llamar para dibujar la ruta detallada
+          drawRoute([latitud, longitud]); // Llama para dibujar la ruta detallada
         }
       }
     } catch (error) {
@@ -110,26 +125,49 @@ export function Template1() {
     }
   };
 
+  // Dibuja la ruta en el mapa
   const drawRoute = async (destinationCoords) => {
     try {
-      // Solicitud a la API de Mapbox para obtener la ruta para caminantes
       const response = await axios.get(`https://api.mapbox.com/directions/v5/mapbox/walking/${initialLocation[1]},${initialLocation[0]};${destinationCoords[1]},${destinationCoords[0]}`, {
         params: {
           access_token: MAPBOX_API_KEY,
           geometries: 'geojson',
         },
       });
-  
+
       const route = response.data.routes[0].geometry.coordinates;
-      const coordinates = route.map(coord => [coord[1], coord[0]]); // Cambiar de [lng, lat] a [lat, lng]
-  
+      const coordinates = route.map(coord => [coord[1], coord[0]]);
+
       setRouteCoords(coordinates); // Almacenar la ruta para dibujarla en el mapa
+      setAwaitingConfirmation(true); // Inicia la espera de confirmación al llegar al destino
+      speakMessage("La ubicación es correcta? Responda con 'Sí es correcta' o 'No es correcta' para continuar.");
     } catch (error) {
       console.error("Error al obtener la ruta de Mapbox Directions API:", error);
     }
   };
-  
 
+  // Maneja la respuesta de confirmación del usuario
+  const handleConfirmationResponse = (response) => {
+    const lowerResponse = response.toLowerCase();
+
+    if (lowerResponse.includes("sí es correcta")) {
+      speakMessage("Perfecto!. Muchas gracias y suerte en su aventura!");
+      resetMap();
+    } else if (lowerResponse.includes("no es correcta")) {
+      speakMessage("Lo siento, intentemos de nuevo. Por favor, indique otra ubicación.");
+      resetMap();
+    }
+  };
+
+  // Resetea el mapa al estado inicial
+  const resetMap = () => {
+    setDestination(null);
+    setRouteCoords([]);
+    setHighlightedNavItem("");
+    setAwaitingConfirmation(false);
+  };
+
+  // Activa o desactiva la escucha por voz
   const handleListener = () => {
     if (isListening) {
       stopListening();
@@ -140,75 +178,64 @@ export function Template1() {
 
   return (
     <>
-      <Timer time={3000} route={'/TotemAdvertising'} />
-      <TotemWebCamera />
-      <section className="relative block h-[50vh] bg-gray-900">
-        <div className="bg-profile-background absolute top-0 h-full w-full">
-          <figure className="relative h-full w-full">
-            <Carrusel className="carrusel" images={imagesFinal == null ? pics : imagesFinal} data={imagesFinal} />
-          </figure>
-        </div>
-      </section>
+      <div className="relative h-screen w-screen">
+        {/* Carrusel de fondo */}
+        <Carrusel className="absolute inset-0 h-full w-full z-0" images={imagesFinal == null ? pics : imagesFinal} data={imagesFinal} />
 
-      <section className="relative translate-y-12 bg-blue-gray-50/50 px-4 py-16">
-        <div className="container mx-auto">
-          <div className="relative -mt-64 mb-6 flex w-full min-w-0 flex-col break-words rounded-3xl bg-white shadow-xl shadow-gray-500/5">
-            <div className="px-6">
-              <div className="my-8 text-center">
-                <Typography variant="h2" color="blue-gray" className="mb-2">Bienvenidos</Typography>
+        <div className="absolute inset-0 bg-opacity-50 z-10 flex flex-col items-center justify-center">
+          <Timer time={3000} route={'/TotemAdvertising'} />
+          <TotemWebCamera />
 
-                <div className="flex items-center justify-center gap-2">
-                  <MapPinIcon className="-mt-px h-4 w-4 text-blue-gray-700" />
-                  <Typography className="font-medium text-blue-gray-700">Cochabamba, Bolivia</Typography>
-                </div>
+          <Typography variant="h2" color="white" className="mb-4">Bienvenidos</Typography>
 
-                <div className="flex justify-center space-x-4 mt-4">
-                  {keywords.map((item) => (
-                    <button
-                      key={item}
-                      className={`px-4 py-2 font-medium ${highlightedNavItem === item ? 'bg-green-500 text-white' : ''}`}
-                    >
-                      {item}
-                    </button>
-                  ))}
-                </div>
+          <div className="flex items-center justify-center gap-2 mb-4">
+            <MapPinIcon className="h-6 w-6 text-white" />
+            <Typography className="font-medium text-white">Cochabamba, Bolivia</Typography>
+          </div>
 
-                <div className="w-full h-96 mt-8">
-                  <MapContainer center={initialLocation} zoom={13} style={{ height: "100%", width: "100%" }}>
-                    <TileLayer
-                      url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                    />
+          {/* Navbar de palabras clave, resaltado según búsqueda por voz */}
+          <div className="flex justify-center space-x-4 mt-4">
+            {keywords.map((item) => (
+              <span
+                key={item}
+                className={`px-4 py-2 font-medium ${highlightedNavItem === item ? 'bg-green-500 text-white' : ''}`}
+              >
+                {item}
+              </span>
+            ))}
+          </div>
 
-                    <Marker position={initialLocation}>
-                      <Popup>Ubicación inicial de la aplicación</Popup>
-                    </Marker>
-                    {destination && (
-                      <Marker position={destination}>
-                        <Popup>Destino: {highlightedNavItem}</Popup>
-                      </Marker>
-                    )}
-                    {/* Dibujar la ruta si existe */}
-                    {routeCoords.length > 0 && (
-                      <Polyline positions={routeCoords} color="blue" />
-                    )}
-                  </MapContainer>
-                </div>
+          {/* Contenedor del Mapa */}
+          <div className="w-4/5 h-2/3 mt-6">
+            <MapContainer center={initialLocation} zoom={13} style={{ height: "100%", width: "100%" }}>
+              <TileLayer
+                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+              />
 
-                <div className="mb-6">
-                  <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900">
-                    Presiona el micrófono y di a dónde te gustaría ir...
-                  </label>
-                  <button onClick={handleListener} className="bg-green-500 text-white font-bold py-2 px-4 rounded">
-                    {isListening ? 'Escuchando...' : 'Iniciar'}
-                  </button>
-                  {browse && <div>{browse}</div>}
-                </div>
-              </div>
-            </div>
+              <Marker position={initialLocation}>
+                <Popup>Ubicación inicial de la aplicación</Popup>
+              </Marker>
+              {destination && (
+                <Marker position={destination}>
+                  <Popup>Destino: {highlightedNavItem}</Popup>
+                </Marker>
+              )}
+              {/* Dibujar la ruta si existe */}
+              {routeCoords.length > 0 && (
+                <Polyline positions={routeCoords} color="blue" />
+              )}
+            </MapContainer>
+          </div>
+
+          {/* Botón para iniciar la escucha por voz */}
+          <div className="mt-6">
+            <button onClick={handleListener} className="bg-green-500 text-white font-bold py-2 px-4 rounded">
+              {isListening ? 'Escuchando...' : 'Iniciar'}
+            </button>
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 }
